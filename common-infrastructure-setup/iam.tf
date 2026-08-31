9ea85a92d9fb45a922897ee6d9d8c54f70eabe17
geethanjali <geethanjalimgangisetty@gmail.com>
2026-08-31 15:39:07 +0530

    removing the oidc block and modifying the role accordingly

--- common-infrastructure-setup/iam.tf
+++ common-infrastructure-setup/iam.tf
@@ -1,39 +1,47 @@
+# 1. Custom Policy for GitHub Runner
 module "iam_policy" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
   version = "= 6.8.0"
-  name        = "github_runner_access_policy"
-  description = "custom policy for github runner to access the aws resource"
 
-  # Define the actual policy permissions
+  name        = "github_runner_access_policy"
+  description = "Custom policy for github runner to access aws resources"
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = [
-            "s3:*",
-            "dynamodb:*"
-        ]
         Effect   = "Allow"
+        Action   = [
+          "s3:*",
+          "dynamodb:*"
+        ]
         Resource = "*"
       }
     ]
   })
+
   tags = {
     dataClass = "infrastructure-pipeline"
   }
 }
-# Role created to provide github runner to access aws for planning and deploying
+
+# 2. Role for GitHub Runner
 module "iam_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role"
   version = "= 6.0.0"
-  name = "github_runner_access_role"
+
+  name        = "github_runner_access_role"
   description = "This role is created for the github runner to access aws for deployment"
-  enable_github_oidc = true
-  oidc_wildcard_subjects = ["repo:${var.github_repo_name}:*"] #try to pass run time
-  # Attach managed or custom policies
+
+  # Enforces GitHub OIDC Trust Policy construction
+  enable_github_oidc     = true
+  oidc_wildcard_subjects = ["repo:${var.github_repo_name}:*"]
+
+  # Attach custom policy as a map
   policies = {
     github_runner_policy = module.iam_policy.arn
   }
+
   tags = {
     dataClass = "infrastructure-pipeline"
   }
